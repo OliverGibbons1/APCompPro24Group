51b6d65cc3901933a5af5bba0c89494bd452fa9c
jemrules <evern.mccullough@comcast.net>
2024-02-26 07:39:28 -0700

    Update MapEditor2.py

--- MapEditor/MapEditor2.py
+++ MapEditor/MapEditor2.py
@@ -22,7 +22,7 @@ from math import sin, pi
 
 # All Individual Images
 
-# MapTilesRef 1D array of Strings Refrencing tile filenames
+# MapTilesRef 1D list of Strings Refrencing tile filenames
 # MapTiles 2D array of Integers Refrencing MapTilesRef
 # ObjectTiles 2D array of Strings Refrencing object
 # EventTiles 2D array of Strings Refrencing event
@@ -73,3 +73,13 @@ class Enemy:
 		self.y = y
 	def draw(self, screen):
 		screen.blit(self.img, (self.x, self.y))
+
+class MapEditor:
+	def __init__(self,screen):
+		self.screen = screen
+		self.width = 800
+		self.height = 600
+		self.running = True
+		self.tile = pg.image.load('tile.png')
+		self.tile = scale(self.tile, (32, 32))
+		self.map = []
